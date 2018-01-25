7150b389fcff6e7835a4e8a81958d8140f3b6179
PaulWilson <Wilsop98@gmail.com>
2018-01-25 20:47:32 +0000

    Enncryption complete!
    Program now does one encyption method, Decytpion to follow then other methods

--- Encyption/Encyption/MainWindow.xaml.cs
+++ Encyption/Encyption/MainWindow.xaml.cs
@@ -24,5 +24,85 @@ namespace Encyption
         {
             InitializeComponent();
         }
+
+
+
+        public string output = "";
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            OutputBox.Text = "";
+            string input = "";
+            input = InputBox.Text;
+            input = input + " ";
+            input = input.ToLower();
+            E1Method(input);
+            OutputBox.Text = output;
+        }
+
+        public string E1Method(String input)
+
+        {
+            ///Loops through each letter in the input word and then 
+            ///increases a counter and adds each letter to a different variable
+            ///when it recieves a space character it will stop counting then run 
+            ///the Encrypt method
+            var temp = "";
+            int counter = 0;
+            string word;
+            foreach (char c in input)
+            {
+                if (c != ' ')
+                {
+                    counter++;
+                    temp += c;
+                }
+                else
+                {
+                    word = temp.ToString();
+                    E1Encrypt(word, counter);
+                    counter = 0;
+                    word = "";
+                    temp = "";
+                }
+            }
+            return output;
+        }
+
+        public string E1Encrypt(string word, int counter)
+        {
+            string[] Alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            string letter = "";
+
+            int n = 0;
+            foreach (char c in word)
+            {
+                for (int i = 0; i < 26; i++)
+                {
+                    if (c.ToString() == Alphabet[i])
+                    {
+                        n = i;
+                        n = n + counter;
+                        if (n > 26)
+                        {
+                            do
+                            {
+                                n = n - 26;
+                            } while (n > 26);
+
+                        }
+                        letter = Alphabet[n].ToString() ;
+                        output += letter;
+                    }
+                }
+            }
+            output = output + " ";
+            return output;
+        }
     }
 }
+/*
+ * Encryption Style 1 = take word length shift up alphabet by that amount
+ * 
+ * 
+ * 
+ */
